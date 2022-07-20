d19296edb3a38239d5ccc1e6c64fe06231c7aa22
Ihar Khorau <dostup.96@gmail.com>
2022-07-20 17:44:09 +0300

    Create Database, Dao and Repository

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/repository/NotesRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDao
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NotesRepository
+
+class NotesRepositoryImpl(
+    private val dao: NoteDao
+): NotesRepository {
+    override fun getNotes() = dao.getNotes()
+
+    override suspend fun getNoteById(id: Int) = dao.getNoteById(id)
+
+    override suspend fun insertNote(note: Note) = dao.insertNote(note)
+
+    override suspend fun deleteNote(note: Note) = dao.deleteNote(note)
+}
