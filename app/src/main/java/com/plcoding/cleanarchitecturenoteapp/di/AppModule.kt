0300bc2432561a59d02e7be09c6dd33c481dbf7d
Ihar Khorau <dostup.96@gmail.com>
2022-07-25 22:30:31 +0300

    Create AddEditNoteViewModel, Add GetNote(id) use case

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/di/AppModule.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/di/AppModule.kt
@@ -5,10 +5,7 @@ import androidx.room.Room
 import com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDatabase
 import com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository.NotesRepositoryImpl
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NotesRepository
-import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.AddNote
-import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.DeleteNote
-import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotes
-import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NotesUseCases
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -41,7 +38,8 @@ object AppModule {
         return NotesUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NotesUseCases.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NotesUseCases.kt
@@ -3,5 +3,6 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 data class NotesUseCases(
     val getNotes: GetNotes,
     val addNote: AddNote,
-    val deleteNote: DeleteNote
+    val deleteNote: DeleteNote,
+    val getNote: GetNote
 )
